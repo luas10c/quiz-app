460ef6331edee2e6b0bcf9adfbf56d1c9ee655b3
Luciano Alves <luas10c@gmail.com>
2022-01-21 03:43:56 -0300

    Fix bug

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,14 @@ let answeredQuestions = []
 let rightAnswers = 0
 let wrongAnswers = 0;
 let questionAnsered = false;
+let points = 0;
 let loading = true;
+
+const answer_options = {
+  1: 'A', 2: 'B', 3: 'C',
+  4: 'D', 5: 'E', 6: 'F',
+  7: 'G', 8: 'H', 9: 'I'
+}
 
 const questionStatistics = document.querySelector('.question-statistics');
 const copyQuestionStatistics = questionStatistics.cloneNode(true);
@@ -25,71 +32,106 @@ window.addEventListener('load', () => {
     if (!loading) {
       document.querySelector('.loading').remove()
       document.body.appendChild(copyHeader)
-
-      const randomQuestion = Math.floor(Math.random() * (((questions.length - 1) - 0) + (questions.length - 1)) + 1)
-      const question = questions[randomQuestion - 1];
       const title = copyQuestionSection.querySelector('.question > .container > header > h3')
       const date = copyQuestionSection.querySelector('.question > .container > header > p')
-      title.textContent = question.title
-      date.textContent = `Criado em: ${question.created_at.toLocaleDateString('pt-BR')}`;
+      
+      function randomQuestion() {
+        let question;
+        while (true) {
+          const randomNumber = Math.floor(Math.random() * questions.length);
+          if (!answeredQuestions.includes(questions[randomNumber])) {
+            question = questions[randomNumber];
+            break;
+          } else {
+            console.log('Acabou todas questões')
+            break;
+          }
+        }
+        return question;
+      }
 
-      function answerQuestion(event) {
+      function randomQuestionAnswers(question) {
+        const answers = [];
+        while(true) {
+          const randomNumber = Math.floor(Math.random() * question.answers.length)
+          if (!answers.includes(question.answers[randomNumber])) {
+            answers.push(question.answers[randomNumber]);
+          }
+          if (answers.length === question.answers.length) {
+            break;
+          }
+        }
+        return answers;
+      }
+
+      function handleAnswerQuestion(event) {
         const correctlyAnswer = question.answers[event.target.dataset.question.split(',')[1] - 1];
         if (question.answer === correctlyAnswer.value) {
+          const container = document.createElement('div');
+          container.classList.add('answer', 'answer-right');
+          container.innerHTML = `<h2>Parabéns!</h2><p>Você acertou a resposta.</p>`
+          document.body.appendChild(container);
           rightAnswers++
         } else {
+          const container = document.createElement('div');
+          container.classList.add('answer', 'answer-wrong');
+          container.innerHTML = `<h2>Eiita Giovanaaa</h2><p>Você não acertou a resposta.</p>`
+          document.body.appendChild(container);
           wrongAnswers++
         }
         questionAnsered = true;
         answeredQuestions.push(question)
-
+      
         if (questionAnsered) {
           copyQuestionSection.remove();
           document.body.appendChild(copyQuestionStatistics)
         }
       }
 
-      function handleNextQuestion(event) {
-        const randomQuestion = Math.floor(Math.random() * (((questions.length - 1) - 0) + (questions.length - 1)) + 1)
-        const question = questions[randomQuestion - 1];
-        
-        questionAnsered = false;
-        if (!questionAnsered) {
-          question.answers.forEach((answer) => {
-            copyQuestionSection.querySelector('.answers').removeChild(copyQuestionSection.querySelectorAll('.answer')[0])
+      function handleNextQuestion() {
+        const question = randomQuestion();
+        const answerContainer = document.querySelector('.answer');
+        if (question) {
+          title.textContent = question.title
+          date.textContent = `Criado em: ${question.created_at.toLocaleDateString('pt-BR')}`;
+          copyQuestionSection.querySelector('.answers').innerHTML = '';
+          
+          const answers = randomQuestionAnswers(question)
+          answers.forEach((answer, index) => {
             const li = document.createElement('li')
             li.classList.add('answer')
             li.setAttribute('data-question', `${question.id},${answer.id}`);
-            li.innerText = `${options[answer.id]}) - ${answer.value}`
-            li.addEventListener('click', answerQuestion);
+            li.innerText = `${answer_options[answer.id]}) - ${answer.value}`
+            li.addEventListener('click', handleAnswerQuestion);
             copyQuestionSection.querySelector('.answers').appendChild(li)
-            document.body.appendChild(copyQuestionSection)
           })
+          
+          document.body.appendChild(copyQuestionSection)
+          answerContainer.remove();
           copyQuestionStatistics.remove();
+        } else {
+          return;
         }
       }
 
+      const question = randomQuestion();
+      const randomAnswers = randomQuestionAnswers(question);
       const nextQuestion = copyQuestionStatistics.querySelector('footer #next-question')
+      title.textContent = question.title
+      date.textContent = `Criado em: ${question.created_at.toLocaleDateString('pt-BR')}`;
+
       nextQuestion.addEventListener('click', handleNextQuestion);
 
-      const options = {
-        1: 'A',
-        2: 'B',
-        3: 'C',
-        4: 'D'
-      }
-
-      question.answers.forEach((answer) => {
+      randomAnswers.forEach((answer) => {
         copyQuestionSection.querySelector('.answers').removeChild(copyQuestionSection.querySelectorAll('.answer')[0])
         const li = document.createElement('li')
         li.classList.add('answer')
         li.setAttribute('data-question', `${question.id},${answer.id}`);
-        li.innerText = `${options[answer.id]}) - ${answer.value}`
-        li.addEventListener('click', answerQuestion);
+        li.innerText = `${answer_options[answer.id]}) - ${answer.value}`
+        li.addEventListener('click', handleAnswerQuestion);
         copyQuestionSection.querySelector('.answers').appendChild(li)
       })
-      
       document.body.appendChild(copyQuestionSection)
-    }
-  }, 200);
+    } 
+  }, 50);
 });
--- assets/js/questions.js
+++ assets/js/questions.js
@@ -21,7 +21,8 @@ export default [
         value: '1829'
       }
     ],
-    answer: '1969'
+    answer: '1969',
+    points: 60
   },
   {
     id: 2,
@@ -45,6 +46,57 @@ export default [
         value: 'Santos Dumont'
       }
     ],
-    'answer': 'Nikola Tesla'
+    'answer': 'Nikola Tesla',
+    points: 80
+  },
+  {
+    id: 3,
+    title: 'Em qual país foi inventado o chuveiro elétrico',
+    created_at: new Date('2022-01-20 15:28:33'),
+    answers: [
+      {
+        id: 1,
+        value: 'Estados Unidos'
+      },
+      {
+        id: 2,
+        value: 'França'
+      },
+      {
+        id: 3,
+        value: 'Itália'
+      },
+      {
+        id: 4,
+        value: 'Brasil'
+      }
+    ],
+    answer: 'Brasil',
+    points: 80
+  },
+  {
+    id: 3,
+    title: 'Qual maior estado em população do brasil?',
+    created_at: new Date('2022-01-20 15:28:33'),
+    answers: [
+      {
+        id: 1,
+        value: 'Minas Gerais'
+      },
+      {
+        id: 2,
+        value: 'São Paulo'
+      },
+      {
+        id: 3,
+        value: 'Rio de Janeiro'
+      },
+      {
+        id: 4,
+        value: 'Santa Catarina'
+      }
+    ],
+    answer: 'São Paulo',
+    points: 80
   }
 ]
